4ec0e234523120150cfabe38089f0cc2e4b96d42
Victor Ferreira <vitfera@hotmail.com>
2026-04-30 00:25:07 -0300

    atualiza lcode

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -7,6 +7,8 @@ return [
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'Anapolis',
 
+    'app.lcode' => 'pt_BR',
+
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
 
