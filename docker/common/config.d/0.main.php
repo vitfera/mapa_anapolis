dc0be2df38ba500ac2f2ff41fb6995db21abe28c
Victor Ferreira <vitfera@hotmail.com>
2026-04-29 23:09:06 -0300

    atualiza siteDescription

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -2,7 +2,7 @@
 
 return [
     'app.siteName' => 'Cultura em Rede Anápolis',
-    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteDescription' => 'O Cultura em Rede Anápolis é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'Anapolis',
